871a896120ca4d84e3ad9de5eef3afecd4c1cf63
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-05-11 13:40:15 +0300

    add space after consts

--- src/main/java/ru/alex/App.java
+++ src/main/java/ru/alex/App.java
@@ -7,6 +7,7 @@ public class App
 
     private final static String TEST_STRING = "1231";
     private final static String TEST_STRING_2 = "3221";
+
     public static void main( String[] args ) {
         System.out.println(StringHandler.isAnagram(TEST_STRING, TEST_STRING_2));
     }
