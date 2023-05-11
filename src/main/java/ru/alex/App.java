eaf67dac412f4a4ea8cf518c3755ca8515b1b04c
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-05-11 13:12:46 +0300

    rename variable a to arrayToReverse

--- src/main/java/ru/alex/App.java
+++ src/main/java/ru/alex/App.java
@@ -9,19 +9,19 @@ import java.util.Arrays;
 public class App 
 {
     public static void main( String[] args ) {
-        Integer[] a = new Integer[] {1, 2};
-        System.out.println(Arrays.toString(a));
+        Integer[] arrayToReverse = new Integer[] {1, 2};
+        System.out.println(Arrays.toString(arrayToReverse));
 
-        reverseArray(a);
-        System.out.println(Arrays.toString(a));
+        reverseArray(arrayToReverse);
+        System.out.println(Arrays.toString(arrayToReverse));
     }
 
-    private static <T> void reverseArray(T[] arr) {
+    private static <T> void reverseArray(T[] arrayToReverse) {
 
-        for (int i = 0, j = arr.length - 1; i < arr.length / 2; i++, j--) {
-            T temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+        for (int i = 0, j = arrayToReverse.length - 1; i < arrayToReverse.length / 2; i++, j--) {
+            T temp = arrayToReverse[i];
+            arrayToReverse[i] = arrayToReverse[j];
+            arrayToReverse[j] = temp;
         }
 
     }
