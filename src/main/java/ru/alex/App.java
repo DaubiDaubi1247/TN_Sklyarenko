edaafa43ab2882eab898b0ab546c761085bce691
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-05-11 13:35:34 +0300

    create test variables

--- src/main/java/ru/alex/App.java
+++ src/main/java/ru/alex/App.java
@@ -1,7 +1,13 @@
 package ru.alex;
 
-public class App 
+import ru.alex.utils.StringHandler;
+
+public class App
 {
+
+    private final static String TEST_STRING = "1231";
+    private final static String TEST_STRING_2 = "3221";
     public static void main( String[] args ) {
+        System.out.println(StringHandler.isAnagram(TEST_STRING, TEST_STRING_2));
     }
 }
