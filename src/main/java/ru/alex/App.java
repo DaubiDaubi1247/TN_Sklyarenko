60d3c239c8182595e507f623791fba1ef1ab13e9
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-05-15 12:01:33 +0300

    init

--- src/main/java/ru/alex/App.java
+++ src/main/java/ru/alex/App.java
@@ -7,13 +7,7 @@ package ru.alex;
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println(isPalindrome("1234321"));
+
     }
 
-    private static boolean isPalindrome(String str) {
-
-        String reverseString = new StringBuilder(str).reverse().toString();
-
-        return str.equals(reverseString);
-    }
 }
