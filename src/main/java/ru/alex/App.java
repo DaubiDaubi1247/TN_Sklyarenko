44516902601a64dc47e7fc0cb5de60f9278ab59e
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-05-15 13:13:29 +0300

    create getNumSumBySelectionCondition method

--- src/main/java/ru/alex/App.java
+++ src/main/java/ru/alex/App.java
@@ -3,12 +3,19 @@ package ru.alex;
 import ru.alex.utils.ListHandler;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 
 public class App 
 {
     public static void main( String[] args ) {
-        ListHandler.printRepeatedElements(List.of(1,2,3,1,2,3));
+        List<Integer> numList = List.of(11, 1, 2, 3, -9, 10, 4, -8, -6, 6, -7);
+
+        Predicate<Integer> moreThanZero = num -> num > 0;
+        System.out.println(ListHandler.getNumSumBySelectionCondition(numList, moreThanZero));
+
+        Predicate<Integer> lessThanZero = num -> num < 0;
+        System.out.println(ListHandler.getNumSumBySelectionCondition(numList, lessThanZero));
     }
 
 
