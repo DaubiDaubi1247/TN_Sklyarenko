00fe2f1ef1e0adacca0c6ff8a12dd633144a0c91
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-05-18 23:21:34 +0300

    create user entity

---
+++ src/main/java/ru/alex/tncrud/entity/User.java
@@ -0,0 +1,40 @@
+package ru.alex.tncrud.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "first_name")
+    @NotBlank
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotBlank
+    private String lastName;
+
+    @Column(name = "email")
+    @Email
+    private String email;
+
+    @Column(name = "password")
+    @NotBlank
+    private String password;
+}
