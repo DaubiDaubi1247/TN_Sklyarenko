9597b93711abc04aee5ff19cb8d80d1b923bb49a
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-05-19 09:32:09 +0300

    create passwordEncoder bean

---
+++ src/main/java/ru/alex/tncrud/config/BeanConfig.java
@@ -0,0 +1,13 @@
+package ru.alex.tncrud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class BeanConfig {
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
