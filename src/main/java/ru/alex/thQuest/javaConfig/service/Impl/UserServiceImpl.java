190c9db07721db6db659fd91db33507b497ada04
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-06-04 19:14:22 +0300

    delete component from javaConfig service

--- src/main/java/ru/alex/thQuest/javaConfig/service/Impl/UserServiceImpl.java
+++ src/main/java/ru/alex/thQuest/javaConfig/service/Impl/UserServiceImpl.java
@@ -1,10 +1,8 @@
 package ru.alex.thQuest.javaConfig.service.Impl;
 
-import org.springframework.stereotype.Component;
 import ru.alex.thQuest.dto.UserDto;
 import ru.alex.thQuest.javaConfig.service.UserService;
 
-@Component
 public class UserServiceImpl implements UserService {
 
     @Override
