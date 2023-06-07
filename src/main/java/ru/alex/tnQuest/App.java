4ddd0ccd7656d9bfc178234e0962588d17df63c7
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-06-07 14:52:06 +0300

    delete component from javaConfig service

--- src/main/java/ru/alex/tnQuest/App.java
+++ src/main/java/ru/alex/tnQuest/App.java
@@ -1,10 +1,15 @@
 package ru.alex.tnQuest;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.alex.tnQuest.xml.service.UserService;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class App {
+
+    private final UserService userService;
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
