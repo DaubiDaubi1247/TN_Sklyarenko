37d98d1137274d3be3bdbf7c53cee8e671b2e5c4
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-06-07 16:20:10 +0300

    add postgres dependecy

--- src/test/java/ru/alex/UserTestApp.java
+++ src/test/java/ru/alex/UserTestApp.java
@@ -1,9 +1,30 @@
 package ru.alex;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import ru.alex.service.Impl.UserServiceImpl;
+import ru.alex.service.UserService;
+
+import java.util.List;
+
 /**
  * Unit test for simple App.
  */
 public class UserTestApp {
 
+    private static final List<String> SURNAMES_FOR_TEST = List.of("Иванов", "Сидоров", "Петров", "Наличный");
+    private static UserService userService;
+
+    @BeforeAll
+    public static void initService() {
+        userService = new UserServiceImpl();
+    }
+
+
+    @Test
+    public void testGetAllUsersSurnames_ReturnCorrectValues() {
+        Assertions.assertEquals(SURNAMES_FOR_TEST, userService.getAllUserSurnames());
+    }
 
 }
