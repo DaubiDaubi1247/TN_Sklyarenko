5ee5a5ab43df9a3e48824d19ff5eef980656914e
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-06-17 12:19:23 +0300

    remake bank repository and service with spring boot data jpa

--- src/main/java/ru/alex/repository/BankRepository.java
+++ src/main/java/ru/alex/repository/BankRepository.java
@@ -1,46 +1,16 @@
 package ru.alex.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.alex.entity.Bank;
-import ru.alex.utils.HibernateSessionFactory;
 
 import java.util.List;
 
-public class BankRepository {
+@Repository
+public interface BankRepository extends JpaRepository<Bank, Integer> {
 
-    private static final String GET_ALL_BANK_SQL = "SELECT * " +
-            "FROM bank";
-
-    public static List<Bank> updateAllBanks(String newBankName) {
-
-        try (var session = HibernateSessionFactory.getSessionFactory().openSession()) {
-            session.beginTransaction();
-
-            var bankList = getAllBanks();
-
-            for (Bank bank : bankList) {
-                bank.setName(newBankName);
-                session.merge(bank);
-            }
-
-            session.getTransaction().commit();
-
-        }
-
-        return getAllBanks();
-    }
-
-    public static List<Bank> getAllBanks() {
-
-        List<Bank> bankList;
-
-        try (var session = HibernateSessionFactory.getSessionFactory().openSession()) {
-            session.beginTransaction();
-
-            bankList = session.createNativeQuery(GET_ALL_BANK_SQL, Bank.class).getResultList();
-
-            session.getTransaction().commit();
-        }
-
-        return bankList;
-    }
+    @Query("UPDATE Bank b " +
+            "SET b.name = :newBankName ")
+    List<Bank> updateAllBanks(String newBankName);
 }
--- src/main/java/ru/alex/service/Impl/BankServiceImpl.java
+++ src/main/java/ru/alex/service/Impl/BankServiceImpl.java
@@ -1,15 +1,21 @@
 package ru.alex.service.Impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.alex.entity.Bank;
 import ru.alex.repository.BankRepository;
 import ru.alex.service.BankService;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class BankServiceImpl implements BankService {
+
+    private final BankRepository bankRepository;
 
     @Override
     public List<Bank> updateAllBankNames(String newBankName) {
-        return BankRepository.updateAllBanks(newBankName);
+        return bankRepository.updateAllBanks(newBankName);
     }
 }
